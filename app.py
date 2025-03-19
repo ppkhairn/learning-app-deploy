1611eaeba7adafe5e3cc86360ebc06a56e86812a
pushkarDRS <pushkar.khairnar@deriskingstrategies.com>
2025-03-19 16:06:28 -0500

    modify app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 from src.simple_aws_assets import AwsAssets
 
 # Streamlit UI
-st.title("Simple Number Inputs App") # Title lines
+st.title("Simple Number Inputs App")
 
 # Input fields for two numbers
 num1 = st.number_input("Enter first number:", value=0.0)
